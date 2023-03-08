aa7ac5c4c321069af5a3be94870c0ebb628ec4d5
Alexandre Novaes <ale.oliveira00@gmail.com>
2023-03-08 20:23:14 -0300

    main

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-import MainContent from './components/MainContent';
+
 
 import "../src/styles/main.sass";
 
